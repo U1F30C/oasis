aa05689d3de16ef43c3ef06a219a489443bc01f1
eric <56367967+U1F30C@users.noreply.github.com>
2026-05-16 13:33:21 -0600

    load env file

--- src/server/llm.ts
+++ src/server/llm.ts
@@ -2,7 +2,7 @@ import { createOllama } from "ollama-ai-provider-v2";
 import { generateText } from "ai";
 
 const ollama = createOllama({
-  baseURL: process.env.OLLAMA_BASE_URL ?? "http://192.168.1.4:11434/api",
+  baseURL: process.env.OLLAMA_BASE_URL,
 });
 
 export const qwen3wen3_8b = ollama("qwen3:8b");
